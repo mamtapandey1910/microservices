1b486216676398efd80c1e224ebdbb5a532b53df
Mamta Pandey <mamtapandey@Mamtas-MacBook-Air.local>
2023-09-05 18:47:40 +0800

    initial setup for asset

--- asset/src/allAPI/userRegistration/regRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { userRegistration } from './regController'
-
-const Router = express.Router()
-
-Router.post('/user/registration', userRegistration)
-
-module.exports = Router
--- asset/src/allAPI/userRegistration/regTypes.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from "express"
-
-export type AllAPI = (req: Request, res: Response, next: NextFunction) => Promise<void> | void

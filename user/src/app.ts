c2b9b4e27365d5ef1c3f6c6c63935a0bdd312819
Mamta Pandey <mamtapandey@Mamtas-MacBook-Air.local>
2023-09-05 18:38:15 +0800

    initial commit

--- user/src/app.ts
+++ user/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { errorMiddleware } from './allAPI/middleware/errorMiddleware'
+import { database } from './mongodb'
 
 export const app = express()
 
 // app.use()
+database()
 
 app.use('/', require('./allAPI/userRegistration/regRouter'))
 
